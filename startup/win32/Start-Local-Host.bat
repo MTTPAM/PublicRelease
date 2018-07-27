bc080bd2e31a2c0fef33904ea4f45bd10bd7f30e
linktlh <linktlh@gmail.com>
2018-07-27 02:02:58 -0500

    Explain the launcher a bit and fix start local host

--- startup/win32/Start-Local-Host.bat
+++ startup/win32/Start-Local-Host.bat
@@ -1,10 +1,10 @@
 @echo off
 rem path/to/cmds
-start "start_astron_cluster.bat"
+start start_astron_cluster.bat
 
-start "start_uberdog_server.bat"
+start start_uberdog_server.bat
 
-start "start_ai_server_auto.bat"
+start start_ai_server_auto.bat
 
 cd ../../
 
